34a0d7e0e4cef16f43b828e6be82e3736941065e
Jesús María <infojeluchu@gmail.com>
2018-11-12 18:59:37 +0100

    INITIAL COMMIT

---
+++ 01- CalculaSueldo/CalculaSueldo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaSueldo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DECLARACIÓN
+            int horasTrabajadas;
+            float precioHora;
+            float sueldo;
+            string linea;
+            string e = Encoding.Default.GetString(new byte[] { 128 });
+
+            // ESCRIBIR Y LEER
+            Console.Write("Introduce las horas trabajadas: ");
+            linea = Console.ReadLine();
+
+            // CONVERTIR CADENA EN NÚMERO
+            horasTrabajadas = int.Parse(linea);
+
+            Console.Write("Introduce el precio por hora: ");
+            linea = Console.ReadLine();
+
+            precioHora = float.Parse(linea);
+            sueldo = horasTrabajadas * precioHora;
+            
+            Console.Write("El sueldo total que ganarás es de: ");
+
+            // CODIFICANDO LOS EL STRING 'e'
+            Console.OutputEncoding = Encoding.Default;
+
+            Console.Write(sueldo + e);
+            Console.ReadKey();
+        }
+    }
+}
