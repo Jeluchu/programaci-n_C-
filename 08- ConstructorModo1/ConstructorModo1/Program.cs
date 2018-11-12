1b26a28d43bad2f9735ec1c5e45f347d59ffb11e
Jesús María <infojeluchu@gmail.com>
2018-11-12 20:03:30 +0100

    08- CONSTRUCTOR MODO 1

---
+++ 08- ConstructorModo1/ConstructorModo1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorModo1
+{
+    class Operarios
+    {
+        private int[] sueldos;
+        String lineaLeida;
+        int contador;
+
+        // CONSTRUCTOR
+        public Operarios()
+        {
+            // CANTIDAD DE ESPACIO EN SUELDOS
+            sueldos = new int[5];
+                        
+            for (int i = 0; i < sueldos.Length; i++)
+            {
+                contador = i + 1;
+                Console.Write("Ingrese el sueldo " + contador + ": ");
+                lineaLeida = Console.ReadLine();
+                sueldos[i] = int.Parse(lineaLeida);
+            }
+        }
+
+        public void Imprimir()
+        {
+            // RECORREMOS SUELDOS
+            for (int i = 0; i < sueldos.Length; i++)
+            {
+                Console.WriteLine(sueldos[i]);
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            // CREACIÓN DEL OBJETO
+            Operarios op = new Operarios();
+
+            Console.Write("\n");
+
+            // EL OBJETO LLAMA AL MÉTODO
+            op.Imprimir();
+        }
+    }
+}
