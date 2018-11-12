2b668d73fb9e8ae650db68d870647d9594de278e
Jesús María <infojeluchu@gmail.com>
2018-11-12 19:13:36 +0100

    02 - SUPERFICIE CUADRADO

---
+++ 02- SuperficieCuadrado/SuperficieCuadrado/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperficieCuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String lineaLeida;
+            int lado;
+            int resultado;
+
+            Console.Write("Introduce el tamaño del lado: ");
+            lineaLeida = Console.ReadLine();
+            
+            lado = int.Parse(lineaLeida);
+
+            resultado = lado * lado;
+
+            Console.Write("La superficie del cuadrado es de: " + resultado);
+
+            // ESPERA SALTO DE LÍNEA
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
