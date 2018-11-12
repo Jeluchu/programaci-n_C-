992fec875c68609324479c937e1a2d67c4a49866
Jesús María <infojeluchu@gmail.com>
2018-11-12 19:46:32 +0100

    07- NOTAS ALUMNADO

---
+++ 07- NotasAlumnado/NotasAlumnado/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotasAlumnado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int puntuacion;
+            String lineaLeida;
+
+            Console.Write("Introduce tu nota del examen: ");
+            lineaLeida = Console.ReadLine();
+            puntuacion = int.Parse(lineaLeida);
+
+            if (puntuacion < 5)
+            {
+                Console.Write("Estás suspenso");
+            }
+            else
+            {
+                Console.Write("Has aprobado el examen. ¡Enhorabuena!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
