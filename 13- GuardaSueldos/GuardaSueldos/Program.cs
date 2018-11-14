866930bb2446717dd9ad6fb6ceaa91c0456ec7ec
Jesús María <infojeluchu@gmail.com>
2018-11-14 16:27:18 +0100

    13- GUARDA SUELDOS

---
+++ 13- GuardaSueldos/GuardaSueldos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuardaSueldos
+{
+    class Program
+    {
+        private int[] sueldos;
+        String lineaLeida;
+
+        public void Cargar()
+        {
+            sueldos = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Introduce el valor del sueldo: ");
+                lineaLeida = Console.ReadLine();
+                sueldos[i] = Int32.Parse(lineaLeida);
+            }
+            Console.Write("\n\n");
+        }
+
+        public void Imprimir()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(sueldos[i]);
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Program sueldo1 = new Program();
+            sueldo1.Cargar();
+            sueldo1.Imprimir();
+        }
+    }
+}
