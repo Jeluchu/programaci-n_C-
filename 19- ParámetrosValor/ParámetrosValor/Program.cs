c67be5f05892085d3353e14643f8cd7c56113a6f
Jesús María <infojeluchu@gmail.com>
2018-11-15 20:25:21 +0100

    19- PARAMETROS VAR

---
+++ 19- ParámetrosValor/ParámetrosValor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParámetrosValor
+{
+    class Program
+    {
+        public void MostrarRango(int menor, int mayor)
+        {
+            for(var x = menor; x <= mayor; x++)
+            {
+                Console.Write(x + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.MostrarRango(1, 25);
+            Console.ReadKey();
+        }
+    }
+}
