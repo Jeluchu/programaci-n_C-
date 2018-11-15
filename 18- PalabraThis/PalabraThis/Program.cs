d00ecdc2dbe8a6b295936daa2470b81ecf1b3720
Jesús María <infojeluchu@gmail.com>
2018-11-15 19:03:15 +0100

    18- PALABRA THIS

---
+++ 18- PalabraThis/PalabraThis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalabraThis
+{
+    class Program
+    {
+        private String nombre;
+        private int edad;
+
+        public Program(String nombr, int edd)
+        {
+            this.nombre = nombr;
+            this.edad = edd;
+            this.Imprimir();
+        }
+
+        public void Imprimir()
+        {
+            Console.Write("Nombre: " + this.nombre + "\n");
+            Console.Write("Edad: " + this.edad + "\n");
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Program personal = new Program("Jelu", 19);
+        }
+    }
+}
