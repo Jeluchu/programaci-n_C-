ce352534e93d0b56b6fb12204271cd7ca8c6b199
Jesús María <infojeluchu@gmail.com>
2018-11-15 17:59:53 +0100

    16- RECURSIVIDAD

---
+++ 16- Recursividad/Recursividad/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividad
+{
+    class Program
+    {
+        /*
+         * FUNCIÓN REPETIR ES RECURSIVA
+         * AL EJECUTAR SE BLOQUEARÁ Y EMITIRÁ UN ERROR
+         */ 
+
+        void Repetir()
+        {
+            Repetir();
+        }
+
+        static void Main(string[] args)
+        {
+            Program re = new Program();
+            re.Repetir();
+        }
+    }
+}
