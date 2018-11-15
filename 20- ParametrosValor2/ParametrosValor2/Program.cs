8d72cade10a13fa3387fcfec66be52d689f18011
Jesús María <infojeluchu@gmail.com>
2018-11-15 20:37:22 +0100

    20- PARAMETRO VALOR

---
+++ 20- ParametrosValor2/ParametrosValor2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametrosValor2
+{
+    class Program
+    {
+        public int Mayor(int v1, int v2, int v3)
+        {
+            if (v1 >= v2 && v1 >= v3)
+            {
+                return v1;
+            }
+            else
+            {
+                if (v2 >= v3)
+                {
+                    return v2;
+                }
+                else
+                {
+                    return v3;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int x1, x2, x3;
+
+            Program p = new Program();
+
+            Console.Write("Introduce el primer número: ");
+            x1 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Introduce el segundo número: ");
+            x2 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Introduce el tercer número: ");
+            x3 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("\n");
+            Console.Write("El número mayor es " + p.Mayor(x1, x2, x3));
+            Console.ReadKey();
+
+                       
+        }
+    }
+}
